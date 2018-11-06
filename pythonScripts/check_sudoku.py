81c63992c9bee56f7449d9a2a70544640307568d
Harkirat  Singh <harkirat.hira@gmail.com>
2018-11-06 19:04:39 +0530

    Add files via upload

--- pythonScripts/check_sudoku.py
+++ pythonScripts/check_sudoku.py
@@ -1,3 +1,13 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Nov 28 09:05:10 2017
+
+@author: Harkirat155
+"""
+t = [[1,2,3],
+     [3,1,2],
+     [2,3,1]]
+
 def Check_sudoku(p):
     #makingComparingList
     complist = []
@@ -5,23 +15,6 @@ def Check_sudoku(p):
     while a < len(p):
         complist.append(a + 1)
         a += 1
-    #arranging&checkingColumns
-    d = 0
-    while d < len(p):
-        check = []
-        e = 0
-        while e < len(p):
-            check.append(p[e][d])
-            e += 1
-        xxe = []
-        f = 0
-        while f < len(p):
-            xxe.append(check.pop(check.index(min(check))))
-            f += 1
-        check = xxe
-        if check != complist:
-            return False
-        d += 1
     #arranging&CheckingRow
     for b in p:
         exe = []
@@ -31,4 +24,21 @@ def Check_sudoku(p):
         b = exe
         if b != complist:
             return False
+    #arranging&checkingColumns
+    d = 0
+    while d < len(p):
+        check = []
+        e = 0
+        while e < len(p):
+            check.append(p[d][e])
+            e += 1
+        xxe = []
+        f = 0
+        while f < len(check):
+            xxe.append(check.pop(check.index(min(check))))
+            f += 1
+        check = xxe
+        if check != complist:
+            return False
+        d += 1
     return True #ifEverythingIsFine
